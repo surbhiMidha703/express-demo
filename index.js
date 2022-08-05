fa3e46886ffab420f6655feb1fa210c58bd30c9b
Surbhi Midha <surbhimidha@192.168.20.73>
2022-08-05 15:57:22 +1000

    Iniial commit witgh validation check - joi

---
+++ index.js
@@ -0,0 +1,58 @@
+const Joi = require("joi");
+const express = require("express");
+
+const app = express();
+
+app.use(express.json()); //express.json returns middleware, app.use uses that middleware in the request processing pipeline
+
+const courses = [
+  { id: 1, name: "maths" },
+  { id: 2, name: "english" },
+];
+
+app.get("/", (req, res) => {
+  res.send("hello world!!!");
+});
+
+app.get("/api/courses", (req, res) => {
+  // get courses from db
+  res.send(`courses ${JSON.stringify(courses)}`);
+});
+
+// defining query param:
+app.get("/api/courses/:id", (req, res) => {
+  //   res.send(req.query);
+  //   res.send(req.params);
+
+  const course = courses.find((ele) => ele.id === parseInt(req.params.id));
+
+  !course ? res.status(404).send("Course not found!") : res.send(course);
+});
+
+//post a course and validate incoming name in the request
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const { value, error } = schema.validate(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    // name: req.body.name,
+    name: value.name,
+  };
+
+  courses.push(course);
+  console.log("courses=> ", courses);
+  res.send(course);
+});
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}...!!`);
+});
